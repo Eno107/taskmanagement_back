568951aca9d670d0a69cf53b69aaa460f6badacc
EnoBendi <102754796+Eno107@users.noreply.github.com>
2026-03-16 01:51:04 +0100

    Update dependencies in package.json and package-lock.json, including downgrading Prisma client version and adding dotenv and @nestjs/swagger. Remove unused prisma.config.ts file and update schema.prisma for compatibility. Enhance main.ts to include Swagger documentation setup.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
+  const config = new DocumentBuilder()
+    .setTitle('Task Management API')
+    .setDescription('Real-time task management application API')
+    .setVersion('1.0')
+    .build();
+
   const app = await NestFactory.create(AppModule);
+
+  // Swagger documentation
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+
+void bootstrap();
